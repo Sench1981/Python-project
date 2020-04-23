2e7c6f809e5ce757d3edfcbe6a54b3f78f9f049e
Sench1981 <km@59.ru>
2020-04-23 21:36:55 +0500

    Создал файл со сроковыми функциями Добавил функцию нечетного сравнения

---
+++ StrFunction.py
@@ -0,0 +1,11 @@
+def compare (S1,S2):
+    ngrams=[
+            S1[i:i+3] for i in range(len(S1))
+    ]
+    count=0
+    for ngram in ngrams:
+        count+=S2.count(ngram)
+    return count/max(len(S1),len(S2))
+
+#m=compare("Стол","Стул")
+#print(m)
